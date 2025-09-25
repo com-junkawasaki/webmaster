9faa240cd0f3a8b64643ca1cca81031df310abb2
com-junkawasaki <04.feasts_minded@icloud.com>
2025-09-25 22:01:48 +0900

    mv

---
+++ astro/.astro/content-modules.mjs
@@ -0,0 +1,9 @@
+
+export default new Map([
+["src/content/posts/01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F01.mdx&astroContentModuleFlag=true")],
+["src/content/posts/agent-noun.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fagent-noun.mdx&astroContentModuleFlag=true")],
+["src/content/posts/infomation-is-physical-quantity.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Finfomation-is-physical-quantity.mdx&astroContentModuleFlag=true")],
+["src/content/posts/sentiment-of-japanese.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsentiment-of-japanese.mdx&astroContentModuleFlag=true")],
+["src/content/posts/love-is-self.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flove-is-self.mdx&astroContentModuleFlag=true")],
+["src/content/posts/spirit-in-physics.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fspirit-in-physics.mdx&astroContentModuleFlag=true")]]);
+		
